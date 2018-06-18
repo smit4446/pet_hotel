6151e4c66f920b617fecf4b62497179c6f4c055b
smit4446 <smit4446@gmail.com>
2018-06-18 13:29:09 -0500

    updated sql file

--- petHotel.sql
+++ petHotel.sql
@@ -6,6 +6,7 @@ create table owners (
 
 create table pets (
 	id serial primary key,
+	image_path varchar(80),
 	name varchar(50),
 	owner_id int not null references owners on delete cascade,
 	breed varchar(50),
@@ -14,13 +15,14 @@ create table pets (
 )
 
 insert into owners ("first_name", "last_name")
-values ('Cookie', 'Fleck'), ('Meg', 'Swan'), ('Sherri', 'Ann'), 
-('Stefan', 'Vanderhoof'), ('Harlan', 'Pepper')
+values ('Cookie', 'Fleck'), ('Meg', 'Swan'), 
+('Sherri', 'Ann'), ('Stefan', 'Vanderhoof'), 
+('Harlan', 'Pepper')
 
-insert into pets ("name", "owner_id", "breed", "color", "is_checked_in")
-values ('Beatrice', 1,'Weimaraner', 'brown', 'IN'), 
-('Winky', 2,'Norwich Terrier', 'brown/black', 'IN'), 
-('Hubert', 3, 'Bloodhound', 'brown/black', 'IN'), 
-('Miss Agnes', 4, 'Shih Tzu', 'brown/black', 'IN'), 
-('Tyrone', 4,'Shih Tzu', 'brown/black', 'IN'), 
-('Rhapsody in White', 5,'Standard Poodle', 'white', 'IN')
+insert into pets ("image_path", "name", "owner_id", "breed", "color", "is_checked_in")
+values ('beatrice.jpg', 'Beatrice', 1,'Weimaraner', 'brown', 'IN'), 
+('winky.jpg','Winky', 2,'Norwich Terrier', 'brown/black', 'IN'), 
+('hubert.jpg','Hubert', 3, 'Bloodhound', 'brown/black', 'IN'), 
+('miss-agnus.jpg', 'Miss Agnes', 4, 'Shih Tzu', 'brown/black', 'IN'), 
+('tyrone.jpg', 'Tyrone', 4,'Shih Tzu', 'brown/black', 'IN'), 
+('rhapsody.jpg', 'Rhapsody in White', 5,'Standard Poodle', 'white', 'IN')
